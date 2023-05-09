42766956cf426c407b407017936d319649af8397
afransayyed10 <afransayyed45@gmail.com>
2023-05-09 13:28:40 +0530

    first commit

--- src/Zap.js
+++ src/Zap.js
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Zap.css';
 
-// const ZAPIER_WEBHOOK_URL = 'https://hooks.zapier.com/hooks/catch/14826135/33nfd6b/';
-// const SPREADSHEET_ID = '1Ekc6W1Fj-aEdCENT4tziTq1FepX2GLgY2PkxuCtvSog';
-// const API_KEY = '1065952462273-8gppcnl7busm0aq886afj24ppuduc4mu.apps.googleusercontent.com';
-// const SHEET_NAME = 'testing'
 
 function triggerZap(name, email, message, setData) {
   const data = {
@@ -16,18 +12,6 @@ function triggerZap(name, email, message, setData) {
   
   console.log('Data being sent to Zapier:', data);
 
-// axios.post(ZAPIER_WEBHOOK_URL, data, {
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   })
-//     .then(() => {
-//       console.log('Zap triggered successfully!');
-//       setData(prevData => [...prevData, data]) // push new data to array and update state
-//     })
-//     .catch((error) => {
-//       console.log('Zap trigger failed with error:', error.message);
-//     });
 try {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://hooks.zapier.com/hooks/catch/14826135/3355td9/");
@@ -46,19 +30,7 @@ try {
             }
             // setData(newData);
         }
-        // if (this.readyState === 4 && this.status === 200) {
-        //     try {
-        //       console.log("Response from Zapier:", this.responseText);
-        //       const newData = JSON.parse(this.responseText);
-        //       console.log("New data from Zapier:", newData);
-        //       // setData(Array.isArray(newData) ? newData : []);
-        //       setData(prevData => [...prevData, newData]);
 
-        //     } catch (error) {
-        //       // setData([]);
-        //       console.error("Failed to parse response data:", error.message);
-        //     }
-        // }
     };
   } catch(e) {
     console.error(e);
